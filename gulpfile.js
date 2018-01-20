f61f4ac6110d6c3f89c04eff14ea48fbc924ae4e
Kai Shao <grepug@163.com>
2018-01-20 15:35:34 +0800

    rename wetype-gulpfile to wetype-util

--- gulpfile.js
+++ gulpfile.js
@@ -4,4 +4,4 @@ const { dependencies } = require('./package.json')
 let modules = Object.keys(dependencies)
 let pkgJsons = modules.map(el => `./node_modules/${el}/package.json`)
 
-require('wetype-gulpfile').gulpfile(gulp, pkgJsons)
+require('wetype-util').gulpfile(gulp, pkgJsons)
